adf14e745fe2da4b3b7fa5447a7968f9af3a532a
*q91fTG8Von <kumagikazu@gmail.com>
2022-09-22 10:01:19 +0900

    form_withでフォーム書き換え

--- app/controllers/datas_controller.rb
+++ app/controllers/datas_controller.rb
@@ -1,35 +1,33 @@
 class DatasController < ApplicationController
     def index
         # DBから顧客情報を取得
-        @customers = Customer.all
+        @customers = Customer.all()
+    end
 
-        # エラー回避。デフォルトで定義されていない変数
+    def new
         @customer = Customer.new
-        @form_data = {}
+    end
+
+    def create
+        # モデル作成
+        @customer = Customer.new(customer_params)
+
+
+        # データ保存
+        # if @customer.save
+        #     flash[:success] = "データを追加しました"
+        #     redirect_to('/')
+        # エラー時の挙動 
+        # else
+        #     flash[:danger] = "データの追加に失敗しました"
+        #     render 'datas/index'
     end
 
 
 
-    def create
-        # モデル作成
-        @customer = Customer.new
+    private
 
-        # 値代入
-        @customer[:name] = params[:create_name]
-        @customer[:age] = params[:create_age]
-        @customer[:media] = params[:create_media]
-
-        # データ保存
-        if @customer.save
-            @customer.save
-            flash[:success] = "データを追加しました"
-            redirect_to('/')
-        # エラー時の挙動 
-        else
-            flash[:danger] = "データの追加に失敗しました"
-            index()
-            @form_data = params
-            render 'datas/index'
-        end
+    def customer_params
+      params.require(:fluit).permit(:name, :age, :media)
     end
 end
