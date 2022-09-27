791f7809a5dc10f5cb1f9e172c002c8bf8073156
*q91fTG8Von <kumagikazu@gmail.com>
2022-09-27 20:19:54 +0900

    外部リクエストを可能にした.

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -1,11 +1,11 @@
 class ApisController < ApplicationController
+    skip_before_action :verify_authenticity_token
     
     def show
         render json: {status: 'Sucess', message: "show"}
     end
 
     def create
-        
         render json: {status: 'Sucess', message: 'create'}
     end
 end
